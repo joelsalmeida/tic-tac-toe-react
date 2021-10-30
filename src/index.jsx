94140745c9e915bec2868903634611714751dc12
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-10-30 13:01:08 -0300

    Adiciona interatividade ao componente Square usando setState e constructors

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,19 @@ import './index.css';
 
 // SQUARE COMPONENT
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
